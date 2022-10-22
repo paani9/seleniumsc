42a32d3875c24b1e20db1e4951423f77e12e9f1d
paani9 <tpaani9@gmail.com>
2022-10-23 02:39:36 +0530

    commitseleniumsc

---
+++ src/ocb/Ochb.java
@@ -0,0 +1,11 @@
+package ocb;
+;
+public class Ochb {
+	public static void main(String[] args)
+	{String key="webdriver.gecko.driver";
+	String value="./Software/geckodriver.exe";
+	System.setProperty(key,value);
+	
+	}
+
+}
